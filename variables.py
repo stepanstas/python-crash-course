432088f1585a8de29fa16228334540e6ea2b6689
Stepan Stastny <stepan86@yahoo.com>
2023-08-09 16:15:31 -0600

    Initial commit

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Store a message in a variable and then print that variable 
+
+message = "Hello World! This is a message!"
+print(message)
+
+# Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print that new message 
+
+message = "This is a new message!"
+print(message)
+
+# Store a person's name in a variable, and print a message to that person. Your message should be simple, such as, "Hello Bison, would you like to learn some Python today?"
+
+name = "Bison"
+message = "Hello " + name + ", would you like to learn some Python today?"
+print(message)
+
+# Store a person's name in a variable, and then print that person's name in lowercase, uppercase, and titlecase 
+
+person_name = "Bison"
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
